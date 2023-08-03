d9e32b5500b491181c4f3d11c639de7394b2cd3b
BenjaminN117 <benjaminhewett409@gmail.com>
2023-08-03 11:03:20 +0100

    Feat: Added directory traversing

--- src/main.go
+++ src/main.go
@@ -2,17 +2,81 @@ package main
 
 import (
 	"fmt"
+	"log"
+	"os"
+	"path/filepath"
 )
 
 var filetype = map[string]string{
 
 	"dfs": "dfdf"}
 
-func directory_traverse(filepath string) []string {
-	traversedFiles := []string{"dfkjdfvkjdfv"}
-	fmt.Println(traversedFiles)
+var traversedFiles = []string{}
+
+func printFile(path string, info os.FileInfo, err error) error {
+	// Error handling for directory traverse
+	if err != nil {
+		log.Print(err)
+		return nil
+	}
+	traversedFiles = append(traversedFiles, path)
+	return nil
+}
+
+func directory_checker(fileName string) bool {
+
+	fi, err := os.Lstat(fileName)
+	if err != nil {
+		log.Fatal(err)
+	}
+	switch mode := fi.Mode(); {
+
+	case mode.IsDir():
+		return true
+
+	default:
+		return false
+	}
+}
+
+func removeValueFromSlice(slice []string, value string) []string {
+	index := -1
+	for i, v := range slice {
+		if v == value {
+			index = i
+			break
+		}
+	}
+	if index == -1 {
+		// Value not found in the slice, return the original slice.
+		return slice
+	}
+
+	// Create a new slice by appending elements before and after the value to remove.
+	return append(slice[:index], slice[index+1:]...)
+}
+
+func directory_traverse(directoryPath string) []string {
+	/*
+		Traverses the directory and returns a list of files
+	*/
+
+	// walk the directory
+	err := filepath.Walk(directoryPath, printFile)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	for _, value := range traversedFiles {
+		if directory_checker(value) == true {
+			traversedFiles := removeValueFromSlice(traversedFiles, value)
+		}
+	}
 
 	return traversedFiles
+}
+
+func file_checker() {
 
 }
 
@@ -24,6 +88,6 @@ func main() {
 	// flag.Parse()
 	// fmt.Println(*filepath)
 
-	fmt.Println(directory_traverse("~/"))
+	fmt.Println(directory_traverse("/users/benjamin/Downloads"))
 
 }
